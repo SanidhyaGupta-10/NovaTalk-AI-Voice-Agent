42f47fa27b853d3ff52e527b9a2bf6b3474757ed
Sanidhya Gupta <adityasgupta1857@gmail.com>
2026-02-28 21:19:50 +0530

    feat: Implement core data models for books, voice sessions, and book segments, and add a file upload API route.

--- database/models/book.model.ts
+++ database/models/book.model.ts
@@ -1,19 +1,20 @@
-import { Schema, model, models } from 'mongoose';
+import { model, Schema, models } from "mongoose";
+import {IBook} from "@/types";
 
-const BookSchema = new Schema({
+const BookSchema = new Schema<IBook>({
     clerkId: { type: String, required: true },
     title: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true, lowercase: true, trim: true },
     author: { type: String, required: true },
     persona: { type: String },
     fileURL: { type: String, required: true },
     fileBlobKey: { type: String, required: true },
-    coverURL: { type: String, required: true },
+    coverURL: { type: String },
     coverBlobKey: { type: String },
     fileSize: { type: Number, required: true },
     totalSegments: { type: Number, default: 0 },
 }, { timestamps: true });
 
-const Book = models?.Book || model('Book', BookSchema);
+const Book = models.Book || model<IBook>('Book', BookSchema);
 
 export default Book;
